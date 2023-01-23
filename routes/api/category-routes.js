67e23e615d992f919b0bc1d36893a1d71b6a66f5
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-23 17:46:28 -0600

    get route made for id values

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -14,13 +14,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  
 });
 
 router.put('/:id', (req, res) => {
