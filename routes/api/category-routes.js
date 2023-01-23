c9fe5413e699d7611114c53773fd3543cf1f5179
CASTICRI003 <ccastillo0924@gmail.com>
2023-01-23 17:48:44 -0600

    defined delete route for categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -59,8 +59,21 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const catData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!catData) {
+      res.status(404).json({ message: 'No category with this id!' });
+      return;
+    }
+    res.status(200).json(catData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
